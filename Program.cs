6363db59dda6b1a4a0bef033b7e921a22fc9022b
Kirill <kiril-shitikov@yandex.ru>
2023-05-20 23:03:41 +0300

    docs: first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задайте двумерный массив. Напишите программу которая поменяет первую и последнюю строку массива
+
+Console.WriteLine("Введите число №1");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число №2");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] result = new int[rows, columns];
+
+for (int i = 0; i < result.GetLength(0); i++)
+{
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        result[i, j] = new Random().Next(0, 10);
+    }
+}
+
+
+for (int i = 0; i < result.GetLength(0); i++)
+{
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        Console.Write(result[i, j]);
+    }
+
+    Console.WriteLine();
+}
+
+for (int j = 0; j < result.GetLength(1); j++)
+{
+    int temp = result[0, j];
+    result[0, j] = result[result.GetLength(0) - 1, j];
+    result[result.GetLength(0) - 1, j] = temp;
+}
+
+Console.WriteLine("________________");
+
+for (int i = 0; i < result.GetLength(0); i++)
+{
+    for (int j = 0; j < result.GetLength(1); j++)
+    {
+        Console.Write(result[i, j]);
+    }
+
+    Console.WriteLine();
+}
